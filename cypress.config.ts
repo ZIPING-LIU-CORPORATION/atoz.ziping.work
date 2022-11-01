92c22797375da7ef02e78fe184cde34f0f7cb518
🌝Ziping <dev@ziping.life>
2022-10-31 22:53:21 -0500

    remove artifacts code

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,4 +1,7 @@
 import { defineConfig } from "cypress";
+import path from 'path';
+
+
 
 export default defineConfig({
   projectId: 't7aqz7',
@@ -9,6 +12,12 @@ export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+
+      config.configFile = path.resolve(__dirname, 'cypress.config.ts');
+
+      config.projectId = 't7aqz7';
     },
+  
+    supportFile: path.resolve(__dirname, 'cypress/support/e2e.js')
   },
 });
